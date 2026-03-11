c7ce83b1362e7718802150a6d319482e2ed6beae
Ashumit <135939043+Ash9719@users.noreply.github.com>
2026-03-12 00:51:16 +0530

    Create 10_Self_Correcting_Straight_Line_Robot.ino

---
+++ 10_Self_Correcting_Straight_Line_Robot/10_Self_Correcting_Straight_Line_Robot.ino
@@ -0,0 +1,20 @@
+/*
+ * ElarchiGO Arduino Kit 1.0
+ * Verified code will be pasted here soon!
+ */
+
+// ==========================================
+// PASTE YOUR VERIFIED CODE BELOW THIS LINE
+// ==========================================
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// ==========================================
+// PASTE YOUR VERIFIED CODE ABOVE THIS LINE
+// ==========================================
