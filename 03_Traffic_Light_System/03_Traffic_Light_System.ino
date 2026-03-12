6d8968fe1ec7ef88b4a6d5740087c6e8e62885fb
Ashumit <135939043+Ash9719@users.noreply.github.com>
2026-03-12 14:03:33 +0530

    Update 03_Traffic_Light_System.ino

--- 03_Traffic_Light_System/03_Traffic_Light_System.ino
+++ 03_Traffic_Light_System/03_Traffic_Light_System.ino
@@ -1,20 +1,23 @@
-/*
- * ElarchiGO Arduino Kit 1.0
- * Verified code will be pasted here soon!
- */
-
-// ==========================================
-// PASTE YOUR VERIFIED CODE BELOW THIS LINE
-// ==========================================
-
+// Project 03: Traffic Light System
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(12, OUTPUT); // Green
+  pinMode(11, OUTPUT); // Yellow
+  pinMode(10, OUTPUT); // Red
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  // Green light on for 5 seconds
+  digitalWrite(12, HIGH);
+  delay(5000);
+  digitalWrite(12, LOW);
 
-// ==========================================
-// PASTE YOUR VERIFIED CODE ABOVE THIS LINE
-// ==========================================
+  // Yellow light on for 2 seconds
+  digitalWrite(11, HIGH);
+  delay(2000);
+  digitalWrite(11, LOW);
+
+  // Red light on for 5 seconds
+  digitalWrite(10, HIGH);
+  delay(5000);
+  digitalWrite(10, LOW);
+}
