3b70517674a45c71ec5900a74d597ed17a7372dd
Ashumit <135939043+Ash9719@users.noreply.github.com>
2026-03-12 13:52:03 +0530

    Update 02_Blinking_LED.ino

--- 02_Blinking_LED/02_Blinking_LED.ino
+++ 02_Blinking_LED/02_Blinking_LED.ino
@@ -1,20 +1,13 @@
-/*
- * ElarchiGO Arduino Kit 1.0
- * Verified code will be pasted here soon!
- */
-
-// ==========================================
-// PASTE YOUR VERIFIED CODE BELOW THIS LINE
-// ==========================================
+// Project 02: Blinking LED
 
 void setup() {
-  // put your setup code here, to run once:
+  // Initialize digital pin 13 as an output.
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(13, HIGH);   // Turn the LED on (HIGH is the voltage level)
+  delay(1000);              // Wait for a second (1000 milliseconds)
+  digitalWrite(13, LOW);    // Turn the LED off by making the voltage LOW
+  delay(1000);              // Wait for a second
 }
-
-// ==========================================
-// PASTE YOUR VERIFIED CODE ABOVE THIS LINE
-// ==========================================
